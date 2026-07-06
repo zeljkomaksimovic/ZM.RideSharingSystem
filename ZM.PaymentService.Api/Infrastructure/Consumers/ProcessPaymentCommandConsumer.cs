ec2b93ded9e7dee3e2f672a6fe19c8c45fd9757c
Zeljko Maksimovic <zeljko95m@live.com>
2026-07-06 21:59:09 +0200

    Refactor of the ProcessPaymentCommandConsumer class.

--- ZM.PaymentService.Api/Infrastructure/Consumers/ProcessPaymentCommandConsumer.cs
+++ ZM.PaymentService.Api/Infrastructure/Consumers/ProcessPaymentCommandConsumer.cs
@@ -2,12 +2,11 @@ using MassTransit;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using ZM.PaymentService.Api.Persistence;
-using ZM.RideSharingSystem.Contracts.Events;
 using ZM.PaymentService.Api.Application.UseCases.ProcessPayment;
 
 namespace ZM.PaymentService.Api.Infrastructure.Consumers
 {
-    public class ProcessPaymentCommandConsumer : IConsumer<RideCompletedEvent>
+    public class ProcessPaymentCommandConsumer : IConsumer<ProcessPaymentCommand>
     {
         private readonly ILogger<ProcessPaymentCommandConsumer> _logger;
         private readonly ISender _sender;
@@ -20,18 +19,21 @@ namespace ZM.PaymentService.Api.Infrastructure.Consumers
             _dbContext = dbContext;
         }
 
-        public async Task Consume(ConsumeContext<RideCompletedEvent> context)
+        public async Task Consume(ConsumeContext<ProcessPaymentCommand> context)
         {
             if (await _dbContext.ProcessedMessages.AnyAsync(p =>
                     p.MessageId == context.MessageId &&
                     p.ConsumerName == nameof(ProcessPaymentCommandConsumer),
-                context.CancellationToken))
+                    context.CancellationToken))
             {
                 return;
             }
 
             try
             {
+                //Note: Transaction's are not allowed in In-Memory transport, so we will not use them here.
+                //await _unitOfWork.BeginTransactionAsync(context.CancellationToken);
+
                 await _dbContext.ProcessedMessages.AddAsync(new Persistence.Idempotence.ProcessedMessage
                 {
                     MessageId = context.MessageId,
@@ -39,11 +41,16 @@ namespace ZM.PaymentService.Api.Infrastructure.Consumers
                     CreatedAtUtc = DateTime.UtcNow
                 }, context.CancellationToken);
 
-                // For now amount is unknown; set to 0 and let handler or downstream logic determine actual amount
-                await _sender.Send(new ProcessPaymentCommand(context.Message.RideId, 0m), context.CancellationToken);
+                await _sender.Send(new ProcessPaymentCommand(
+                    context.Message.RideId,
+                    context.Message.Amount), 
+                    context.CancellationToken);
+
+                //await _unitOfWork.CommitAsync(context.CancellationToken);
             }
             catch (Exception ex)
             {
+                //await _unitOfWork.RollbackAsync(context.CancellationToken);
                 _logger.LogError(ex, $"Error processing {nameof(ProcessPaymentCommandConsumer)} with MessageId: {context.MessageId}");
             }
         }
